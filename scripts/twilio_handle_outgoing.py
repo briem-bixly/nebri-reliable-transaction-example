f74cee0c37054ac8b0e41fb9746dec42b5bf0f0f
briem-bixly <briem@bixly.com>
2015-09-17 14:52:11 -0700

    Create twilio_handle_outgoing.py

---
+++ scripts/twilio_handle_outgoing.py
@@ -0,0 +1,22 @@
+from twiliomodels import Message
+from twilioutils import send_message
+
+
+class twilio_handle_incoming(NebriOS):
+    listens_to = ['sms_to', 'sms_from', 'sms_body']
+
+    def check(self):
+        return self.sms_to != '' and\
+               self.sms_from != '' and\
+               self.sms_body != '' and\
+               self.sent == False
+
+    def action(self):
+        try:
+            message_id = send_message(shared.twilio_sid, shared.twilio_token, self.pid)
+            self.twilio_id = message_id
+            self.date_sent = datetime.now()
+            self.sent = True
+        except:
+            # something happened and we got an error... do nothing with this message.
+            pass
