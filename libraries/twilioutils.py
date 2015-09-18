2051756a6d35efc9467e010dcd7910e0182f216d
briem-bixly <briem@bixly.com>
2015-09-18 13:13:54 -0700

    Update twilioutils.py

--- libraries/twilioutils.py
+++ libraries/twilioutils.py
@@ -8,25 +8,26 @@ logging.basicConfig(filename='twilio.log', level=logging.DEBUG)
 
 def get_messages(sid, token):
     try:
-        # in order to keep from retrieving the same messages over and over, we keep track of the last time
-        # we queried twilio
+        # in order to keep from retrieving the same messages over and over,
+        # we keep track of the last time we queried twilio
         fetched = MessageFetch.filter()
         client = TwilioRestClient(sid, token)
         if fetched.length == 0:
             messages = client.messages.list()
         else:
-            # 'after' was found by digging through the api documentation in python-twilio. this has not been tested.
+            # 'after' was found by digging through the api documentation in
+            # python-twilio. this has not been tested.
             # https://twilio-python.readthedocs.org/en/latest/api/rest/resources.html#sms-messages
-            # this can be changed to date_sent with an inequality that will retrieve all messages sent/received after
-            # or before the date depending on inequality. 
+            # this can be changed to date_sent with an inequality that will
+            # retrieve all messages sent/received after or before the date
+            # depending on inequality.
             messages = client.messages.list(after=fetched.date_ran)
         message_count = 0
         for m in messages:
             message_count += 1
-            # twilio doesn't send a json representation back, so we need to make a second query to get storable
-            # data
+            # twilio doesn't send a json representation back,
+            # so we need to make a second query to get storable data
             url = '%s.json' % m.uri
-            logging.debug(url)
             data = requests.get(url, auth=(sid, token)).content
             try:
                 # let's check to see if this message already exists
@@ -49,20 +50,23 @@ def get_messages(sid, token):
                     sms_direction=m.direction,
                     raw_data=data,
                 )
+                if message.sms_direction == 'incoming':
+                    # to help with making sure incorrect scripts aren't triggered
+                    message.sent = True
             message.save()
         if fetched.length == 0:
             # this is the first time we've fetched messages... create an entry
-            MessageFetch(
-                date_ran=datetime.now(),
-                num_messages_fetched=message_count
+            MessageFetch( 
+                date_ran=datetime.now(), 
+                num_messages_fetched=message_count 
             )
-        else:
-            # update the existing entry
-            fetched.date_ran = datetime.now()
-            fetched.num_messages_fetched = message_count
-        fetched.save()
-        return 'Success'
-    except Exception, e:
+        else: 
+            # update the existing entry 
+            fetched.date_ran = datetime.now() 
+            fetched.num_messages_fetched = message_count 
+            fetched.save() 
+        return 'Success' 
+    except Exception, e: 
         return 'Error: %s' % str(e)
 
 
