7d283cc07f8ab54fa3fb5eb42007e0fe2fbc1152
briem-bixly <briem@bixly.com>
2015-09-18 13:11:46 -0700

    Update twilio_handle_outgoing.py

--- scripts/twilio_handle_outgoing.py
+++ scripts/twilio_handle_outgoing.py
@@ -6,13 +6,14 @@ class twilio_handle_outgoing(NebriOS):
     listens_to = ['sms_to', 'sms_from', 'sms_body']
 
     def check(self):
-        return self.sms_to != '' and\
-               self.sms_from != '' and\
-               self.sms_body != '' and\
+        return self.sms_to != '' and \
+               self.sms_from != '' and \
+               self.sms_body != '' and \
+               self.sms_direction != 'incoming' and \
                self.sent == False
-
     def action(self):
         try:
+            # do stuff with the message
             message_id = send_message(shared.twilio_sid, shared.twilio_token, self.pid)
             self.twilio_id = message_id
             self.date_sent = datetime.now()
